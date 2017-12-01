50f2df7fdf28d2f39eedc343b7759c91050cefce
Fabian <accounts@fabianbalsiger.ch>
2017-12-01 19:18:08 +0100

    feat(PickleDataset): not working yet

--- project/data_loader.py
+++ project/data_loader.py
@@ -1,7 +1,7 @@
 import torch
 from torch.utils.data import Dataset
 import numpy as np
-import  torchvision.transforms  as transforms
+import torchvision.transforms  as transforms
 
 
 class DumbDataset(Dataset):
@@ -18,3 +18,64 @@ class DumbDataset(Dataset):
 
     def __len__(self):
         return len(self.images)
+
+
+class PickleDataset(Dataset):
+
+    def __init__(self, data_dir: str, dataset: str):
+        super(PickleDataset, self).__init__()
+        self.data_dir = data_dir
+        self.dataset = dataset
+
+
+
+class flowers(Dataset):
+    def __init__(self, labeled_img_list, labels, captions):
+        self.__data = labeled_img_list
+        self.__labels = labels
+        self.__captions = captions
+        self.__transform = flowers.__transform()
+
+    def __getitem__(self, index):
+        img = self.__transform(Image.open(self.__data[index][0]))
+        lab = self.__labels.index(self.__data[index][1])
+        cap = self.__captions.index(self.__data[index[2]])
+        return img, lab, cap
+
+    def __len__(self):
+        return len(self.__data)
+
+    def resolve_label(self, index):
+        return self.__labels[index]
+
+    def get_label(self, index):
+        return self.__data[index][1]
+
+    def get_labels(self):
+        return self.__labels
+
+    def resolve_caption(self, index):
+        return self.__captions[index]
+
+    def get_caption(self, index):
+        return self.__data[index][2]
+
+    def get_captions(self):
+        return self.__captions
+
+    def get_unique_captions(self):
+        return set(self.__captions)
+
+    def get_data(self):
+        return self.__data
+
+    def get_full_img_name(self, index):
+        return self.__data[index][0]
+
+    @staticmethod
+    def __transform():
+        return transforms.Compose([
+            transforms.Scale((128, 128)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,), (1.0,))
+        ])
