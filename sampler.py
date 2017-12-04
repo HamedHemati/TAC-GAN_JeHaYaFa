1ced38287522344384b8c00b6360a56ea2c30638
neuperc <hemati.hmd@gmail.com>
2017-12-04 11:22:48 +0100

    updated

--- sampler.py
+++ sampler.py
@@ -6,21 +6,22 @@ from model import NetG
 from data_loader import ImTextDataset
 from torch.utils.data import DataLoader
 
-'''
-This is just a random sampler: random noise+random encoded caption
-'''
 
-#data_root = 'Datasets/Oxford Flowers Dataset'
-#imText_ds = ImTextDataset(data_root,dataset='flowers', train=False)
-#dl = DataLoader(imText_ds, shuffle=True, batch_size=1)
+data_root = 'Datasets/Oxford Flowers Dataset'
+imText_ds = ImTextDataset(data_root,dataset='flowers', train=False)
+dl = DataLoader(imText_ds, shuffle=True, batch_size=1)
 
 trans_toPIL = transforms.ToPILImage() 
 noise = Variable(torch.randn(1,100))
 skv = Variable(torch.randn(1,4800))
-path = 'checkpoints/netG__epoch_100.pth'
+path = 'checkpoints/netG__epoch_60.pth'
 netG = NetG()
 netG.load_state_dict(torch.load(path, map_location=lambda storage, loc: storage))
 
-out = netG(noise,skv)
-img = trans_toPIL(out.data[0])
-img.show()
+for i, (img,lbl,cap) in enumerate(dl):
+    if i%10==4:
+        cap = Variable(cap)
+        out = netG(noise,cap)
+        img = trans_toPIL(out.data[0])
+        img.show()
+        break
